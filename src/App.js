a81b74c90abc46d802a9b05eabfdccc4f01c7a12
102sae <95170874+102sae@users.noreply.github.com>
2022-07-04 14:32:53 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword; changes");
+  }, [keyword]); /*keyword가 업데이트 될때만 */
+  useEffect(() => {
+    console.log("I run when 'counter'changes");
+  }, [counter]); /*counter가 업데이트 될때만 */
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text="Hello" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1> {counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
